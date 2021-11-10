e519af43fabf372aa8bb9db3af74ab61268c4d7d
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-11-10 13:36:36 +0000

    updated background image

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const form = document.getElementById('form');
 
 // default weather
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=262ebc10fcbdb48d141acf5027dce0ad`)
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-    cityName.innerText = data.name;
-    date.innerText = new Date().toDateString();
-    description.innerText = data.weather[0].description;
-    temperature.innerText = `${Math.round((data.main.temp) - 273.15)}°C`;
-    document.getElementById('icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+.then(response => response.json())
+.then((data) => {
+  cityName.innerText = data.name;
+  date.innerText = new Date().toDateString();
+  description.innerText = data.weather[0].description;
+  temperature.innerText = `${Math.round((data.main.temp) - 273.15)}°C`;
+  document.getElementById('icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${data.weather[0].description}')`
   });
 
 // weather from search bar
@@ -22,16 +22,15 @@ const fetchWeather = () => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const query = document.getElementById('query').value;
-    console.log(query);
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=262ebc10fcbdb48d141acf5027dce0ad`)
       .then(response => response.json())
       .then((data) => {
-        console.log(data);
         cityName.innerText = data.name;
         date.innerText = new Date().toDateString();
         description.innerText = data.weather[0].description;
         temperature.innerText = `${Math.round((data.main.temp) - 273.15)}°C`;
         document.getElementById('icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${data.weather[0].description}')`
       });
   });
 };
